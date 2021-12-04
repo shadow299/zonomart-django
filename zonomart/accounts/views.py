ab6abb431ceff683321c3878193c23289ed9cd9f
Shadow299 <chandramanish900@gmail.com>
2021-12-04 15:00:10 +0530

    added login function and created dummy dashboard

--- zonomart/accounts/views.py
+++ zonomart/accounts/views.py
@@ -4,6 +4,14 @@ from .forms import RegistrationForm
 from .models import Account
 from django.contrib.auth.decorators import login_required
 
+#verification email imports
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -19,8 +27,22 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
-            messages.success(request, 'Registration successful')
-            return redirect('register')
+
+            #user activation
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html',{
+                'user' : user,
+                'domain':current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            # messages.success(request, 'Thank you for registering with us. we have sent you verification email to your email. Please verify your account.')
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()
 
@@ -41,8 +63,8 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
         if user != None:
             auth.login(request, user)
-            # messages.success(request, "You Are now loggedin")
-            return redirect('home')
+            messages.success(request, "You Are now logged in")
+            return redirect('dashboard')
         else:
             messages.error(request, "Invalid Username or Password")
             return redirect("login")
@@ -52,5 +74,26 @@ def login(request):
 @login_required(login_url = 'login')
 def logout(request):
     auth.logout(request)
-    messages.success(request, "Uou are logged out")
+    messages.success(request, "You are logged out")
     return redirect('login')
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+    
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, "your account is activated.")
+        return redirect('login')
+    else:
+        messages.error(request, "Invalid activation link")
+        return redirect('register')
+
+
+@login_required(login_url = 'login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
--- zonomart/zonomart/settings.py
+++ zonomart/zonomart/settings.py
@@ -138,3 +138,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+#SMTP config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'zonomart9@gmail.com'
+EMAIL_HOST_PASSWORD = 'Passport@1234'
+EMAIL_USE_TLS = True
