34314f712f6bdd70d8dff3e6f04a04c0d8c90739
Shadow299 <chandramanish900@gmail.com>
2021-12-09 14:10:12 +0530

    making place order functions and order number and order page

---
+++ zonomart/orders/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.db.models import fields
+from .models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'phone', 'email', 'address_line_1', 'address_line_2', 'city', 'state', 'country', 'order_note']
