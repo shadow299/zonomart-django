da9a46f51bef266edb7e49274340b7527e6853dd
Shadow299 <chandramanish900@gmail.com>
2021-12-12 14:27:06 +0530

    2factor check for submitting review and customer rsting start fix

--- zonomart/store/views.py
+++ zonomart/store/views.py
@@ -7,6 +7,7 @@ from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Q
 from .forms import ReviewForm
+from orders.models import Order, OrderProduct
 
 # Create your views here.
 def store(request, category_slug = None):
@@ -40,6 +41,11 @@ def store(request, category_slug = None):
 
     return render(request, 'store/store.html', context)
 
+
+
+
+
+
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug = category_slug, slug = product_slug)
@@ -48,12 +54,26 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
     
+    try:
+        orderproduct = OrderProduct.objects.filter(user=request.user, product_id = single_product.id).exists()
+    except OrderProduct.DoesNotExist:
+        orderproduct = None
+
+    reviews = ReviewRating.objects.filter(product_id = single_product.id, status=True)
+
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
+        'orderproduct': orderproduct,
+        'reviews': reviews,
     }
 
     return render(request, 'store/product_detail.html', context)
+
+
+
+
+
 
 def search(request):
     if 'keyword' in request.GET:
