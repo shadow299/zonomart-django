b75a14f1d6e21144d2ed639782ebb8718ac82baa
Shadow299 <chandramanish900@gmail.com>
2021-12-12 13:34:13 +0530

    added review and rating system

--- zonomart/store/models.py
+++ zonomart/store/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.db.models.base import Model
 from category.models import Category
 from django.urls import reverse
+from accounts.models import Account
+
 
 # Create your models here.
 class Product(models.Model):
@@ -21,10 +24,13 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
 
+
 variation_category_coice = (
     ('color','color'),
     ('size','size'),
 )
+
+
 
 class VeriationManager(models.Manager):
     def colors(self):
@@ -32,6 +38,8 @@ class VeriationManager(models.Manager):
 
     def sizes(self):
         return super(VeriationManager, self).filter(variation_category='size', is_active=True)
+
+
 
 class Variation(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -44,5 +52,22 @@ class Variation(models.Model):
 
     def __str__(self):
         return self.variation_value
+
+
+
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
+
 
 
--- zonomart/store/views.py
+++ zonomart/store/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from store.models import Product
+from django.contrib import messages
+from django.shortcuts import redirect, render, get_object_or_404
+from store.models import Product, ReviewRating
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Q
+from .forms import ReviewForm
 
 # Create your views here.
 def store(request, category_slug = None):
@@ -64,3 +66,34 @@ def search(request):
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
+
+
+
+
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == "POST":
+        try:
+            reviews = ReviewRating.objects.get(user__id = request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(request, "Thank You, Your review has been updated")
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get("REMOTE_ADDR")
+                data.product__id = product_id
+                data.user__id = request.user.id
+                data.save()
+                messages.success(request, "Thank You, Your review has been submitted")
+                return redirect(url)
+                
+            
+
+
